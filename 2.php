d449913302a417c1a9d01d02b94ace8e586603a0
Bross Lightyear <mariovpirani@gmail.com>
2021-05-26 22:50:54 -0300

    First commit

---
+++ 2.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>teste</title>
+</head>
+<body style="background-color: #000;">
+<?php print_r($_POST);?>
+
+<script type="text/javascript">
+	window.onload = function(e) {
+	  window.addEventListener('message', event => {
+	  	console.log(event);
+	  }, false);
+	  mario();
+	};
+	function mario () {
+		setInterval(function () {
+			parent.postMessage('testeretorno', '*');
+		}, 3000);
+	}
+</script>
+</body>
+</html>
